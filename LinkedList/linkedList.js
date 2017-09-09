d07cff567e63e2f855b036cef3a9ebb903ec6de9
pranay <pranay.bhawsar@gmail.com>
2017-09-08 23:01:49 -0700

    intial commit for linkedList, adding addFirst function

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,24 @@
+// implementation of linked list
+
+// constructor function for linked list 
+function LinkedList() {
+    this.head = null;
+}
+
+// constructor function for node 
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+
+// function to insert node at begining of the list
+LinkedList.prototype.addFirst = function(data) {
+    var newNode = new Node(data);
+
+    if (this.head) {
+        newNode.next = this.head
+    }
+    this.head = newNode;
+
+}
