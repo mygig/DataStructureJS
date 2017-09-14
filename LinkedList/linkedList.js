a6292ffb414ccfd35338348d93a3d56632b96204
pranay <pranay.bhawsar@gmail.com>
2017-09-14 16:21:04 -0700

    few more methods for linkedList.

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,18 +1,26 @@
 // implementation of linked list
 
-// constructor function for linked list 
+// constructor function for linked list. 
+/**
+ * @constructor
+ */
 function LinkedList() {
     this.head = null;
     this.length = 0;
 }
 
-// constructor function for node 
+// constructor function for node. 
+/**
+ * 
+ * @param {@} data 
+ * @constructor 
+ */
 function Node(data) {
     this.data = data;
     this.next = null;
 }
 
-// function to insert node at begining of the list
+// function to insert node at begining of the list.
 LinkedList.prototype.addFirst = function(data) {
     var newNode = new Node(data);
     if (this.head) {
@@ -22,7 +30,7 @@ LinkedList.prototype.addFirst = function(data) {
     this.length++;
 }
 
-// function to insert node at the end of the list
+// function to insert node at the end of the list.
 LinkedList.prototype.addLast = function(data) {
     var newNode = new Node(data);
     var current = this.head;
@@ -55,7 +63,7 @@ LinkedList.prototype.indexOf = function(data) {
     return -1;
 }
 
-// function to find element at a given index;
+// function to find element at a given index.
 LinkedList.prototype.elementAt = function(index) {
     var current = this.head;
     var counter = 0;
@@ -69,7 +77,11 @@ LinkedList.prototype.elementAt = function(index) {
     return null;
 }
 
-// function to add element at a given index;
+// function to add element at a given index.
+/**
+ * @param{any} data e.g string,number etc.
+ * @param{Number} index e.g number 0,1,2 ... etc. 
+ */
 LinkedList.prototype.addAt = function(data, index) {
     index--;
     var newNode = new Node(data);
@@ -85,12 +97,72 @@ LinkedList.prototype.addAt = function(data, index) {
     current.next = newNode;
     this.length++;
 }
+
+// function to remove element at head.
+LinkedList.prototype.removeAtHead = function() {
+    var current = this.head;
+    if (current) {
+        this.head = current.next;
+        this.length--;
+    } else {
+        throw new Error("list is empty");
+    }
+}
+
+// function to remove element at tail.
+LinkedList.prototype.removeAtTail = function() {
+    var current = this.head;
+    if (current === null) {
+        throw new Error("list is empty");
+    }
+    if (current.next === null) {
+        this.head = null;
+        this.length--;
+        return;
+    }
+    while (current.next.next !== null) {
+        current = current.next;
+    }
+    current.next = null;
+    this.length--;
+}
+
+// function to remove at a given position
+/**
+ * @param{any} e.g accept any data, e.g number, string etc.
+ */
+LinkedList.prototype.removeAt = function(element) {
+    if (this.head === null) {
+        throw new Error("list is empty");
+    }
+    if (this.head.data === element) {
+        this.head = this.head.next;
+        this.length--;
+        return;
+    }
+    var current = this.head;
+    while (current.next !== null) {
+        if (current.next.data === element) {
+            break;
+        }
+        current = current.next;
+    }
+    if (current.next === null) {
+        throw new Error("element not in the list");
+    } else {
+        current.next = current.next.next;
+        this.length--;
+    }
+}
+
 var list = new LinkedList();
-list.addFirst(100);
-list.addFirst(200);
-list.addFirst(300);
-list.addFirst(400);
-list.addFirst(500);
+// list.addFirst(100);
+// list.addFirst(200);
+// list.addFirst(300);
+// list.addFirst(400);
+// list.addFirst(500);
+// list.removeAtTail();
+
 // console.log(list.indexOf(300))
 
 // console.log(list.elementAt(0));
