dfdbb6c3418ed63deebb9cec0d6302ec6774ae4e
pranay <pranay.bhawsar@gmail.com>
2017-11-01 21:33:44 -0700

    bst tree traversal

--- BinarySearchTree/BSTree.js
+++ BinarySearchTree/BSTree.js
@@ -1,12 +1,12 @@
 // constructor function 
 function BST(value) {
-    this.value = value;
+    this.value = value || 50;
     this.left = null;
     this.right = null;
 }
 
 // insert method
-BST.prototype.insert = function (value) {
+BST.prototype.insert = function(value) {
     if (value < this.value) {
         if (!this.left) {
             this.left = new BST(value);
@@ -19,7 +19,7 @@ BST.prototype.insert = function (value) {
 }
 
 // contains method
-BST.prototype.contains = function (value) {
+BST.prototype.contains = function(value) {
     if (value === this.value) {
         return true;
     } else if (value < this.value) {
@@ -34,3 +34,31 @@ BST.prototype.contains = function (value) {
             return this.right.contains(value);
     }
 }
+
+// depth first traversel of bst
+// http://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
+    if (order === 'preorder')
+        iteratorFunc(this.value);
+    if (this.left)
+        this.left.depthFirstTraversal(iteratorFunc, order);
+    if (order === 'inorder')
+        iteratorFunc(this.value);
+    if (this.right)
+        this.right.depthFirstTraversal(iteratorFunc, order);
+    if (order === 'postorder')
+        iteratorFunc(this.value);
+}
+
+var log = function(value) {
+    console.log(value);
+};
+
+var bst = new BST(50);
+bst.insert(10);
+bst.insert(30);
+bst.insert(20);
+bst.insert(44);
+bst.insert(70);
+
+bst.depthFirstTraversal(log, 'inorder');
